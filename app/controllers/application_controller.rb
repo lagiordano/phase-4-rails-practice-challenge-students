1638d10a01dee4de7620dbd43b691596a13995e7
Lauren Giordano <laurengiordano94@gmail.com>
2023-06-21 16:04:14 +1000

    done with practice challenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+private 
+
+def render_unprocessable_entity_response invalid
+    render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+end
+
+def render_not_found_response
+    render json: { error: "Record not found" }, status: :not_found
+end
+
 end
